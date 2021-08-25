69bc13d22ebc12f7b3de78348586382c0d9db947
Robert Stober <rstober@users.noreply.github.com>
2021-08-25 10:48:28 -0800

    Add files via upload

--- install.bash
+++ install.bash
@@ -1,9 +1,6 @@
 #!/usr/bin/env bash
 
-set -x
-set -v
-
-echo "this is the install script"
+set -xv
 
 installdir=/root/.road-runner
 
@@ -17,16 +14,18 @@ wget https://raw.githubusercontent.com/rstober/dotfiles/main/cmshrc .
 wget https://raw.githubusercontent.com/rstober/dotfiles/main/bookmarks-cmsh .
 wget https://raw.githubusercontent.com/rstober/dotfiles/main/du.cmsh .
 wget https://raw.githubusercontent.com/rstober/dotfiles/main/si.cmsh .
+wget https://raw.githubusercontent.com/rstober/dotfiles/main/ansible.cfg .
 
 cp cmshrc /root/.cmshrc
 cp bookmarks-cmsh /root/.bookmarks-cmsh
 cp du.cmsh /root/.cm/cmsh/du.cmsh
 cp si.cmsh /root/.cm/cmsh/si.cmsh
+cp ansible.cfg /root/.ansible.cfg
 
 module load python3
 
 pip install ansible-base
 
-export ANSIBLE_PYTHON_INTERPRETER=/cm/local/apps/python3/bin/python
+#export ANSIBLE_PYTHON_INTERPRETER=/cm/local/apps/python3/bin/python
 
 ansible-galaxy collection install brightcomputing.bcm
