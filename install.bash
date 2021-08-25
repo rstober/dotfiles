45ed18d02a77b06954afe3001c34ae3a393d1cd8
Robert Stober <rstober@users.noreply.github.com>
2021-08-25 13:39:08 -0800

    Add files via upload

--- install.bash
+++ install.bash
@@ -20,6 +20,7 @@ wget https://raw.githubusercontent.com/rstober/dotfiles/main/ansible.cfg .
 wget https://raw.githubusercontent.com/rstober/dotfiles/main/add-user.yaml .
 wget https://raw.githubusercontent.com/rstober/dotfiles/main/run-yum-update.yaml .
 wget https://raw.githubusercontent.com/rstober/dotfiles/main/clone-software-image.yaml .
+wget https://raw.githubusercontent.com/rstober/dotfiles/main/install-gnome-desktop.yaml .
 
 # download playbooks
 
@@ -30,20 +31,27 @@ cp du.cmsh /root/.cm/cmsh/du.cmsh
 cp si.cmsh /root/.cm/cmsh/si.cmsh
 cp ansible.cfg /root/.ansible.cfg
 
-# module load python3
+# install the brightcomputing.bcm collection
+module load python3
+pip install ansible-base
+ansible-galaxy collection install brightcomputing.bcm
 
-# pip install ansible-base
+# must use the system python to use Ansible's built-in yum/dnf
+# export ANSIBLE_PYTHON_INTERPRETER=/usr/bin/python
+# ansible-playbook -ilocalhost, --flush-cache ${installdir}/run-yum-update.yaml
 
-# ansible-galaxy collection install brightcomputing.bcm
+# must use the Bright python package to intercat with cm-python3 intereter for the Bright collection
+# export ANSIBLE_PYTHON_INTERPRETER=/cm/local/apps/python3/bin/python
+# ansible-playbook -ilocalhost, --flush-cache ${installdir}/clone-software-image.yaml
 
 export ANSIBLE_PYTHON_INTERPRETER=/usr/bin/python
 
-ansible-playbook -ilocalhost, --flush-cache ${installdir}/run-yum-update.yaml
+# install B4DS into cloned software image
 
-export ANSIBLE_PYTHON_INTERPRETER=/cm/local/apps/python3/bin/python
+# install gnome desktop in cloned software image
+ansible-playbook -ilocalhost, --flush-cache ${installdir}/install-gnome-desktop.yaml
 
-ansible-playbook -ilocalhost, --flush-cache ${installdir}/clone-software-image.yaml
-
+# create a set of users
 # for user in robert david alice bob charlie edgar frank
 # do
     # ansible-playbook -ilocalhost, --flush-cache --extra-vars "username=$user pass=6b3rl1n5 prof=cloudjob" ${installdir}/add-user.yaml
