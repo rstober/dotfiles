064ed2b6ac339e99d0f3a370beeb2c48c383c3a2
Robert Stober <rstober@users.noreply.github.com>
2021-08-30 07:47:34 -0800

    Add files via upload

--- install.bash
+++ install.bash
@@ -12,21 +12,23 @@ cd $installdir
 rm *
 
 # download dot files
-wget https://raw.githubusercontent.com/rstober/dotfiles/main/cmshrc .
-wget https://raw.githubusercontent.com/rstober/dotfiles/main/bookmarks-cmsh .
-wget https://raw.githubusercontent.com/rstober/dotfiles/main/du.cmsh .
-wget https://raw.githubusercontent.com/rstober/dotfiles/main/si.cmsh .
-wget https://raw.githubusercontent.com/rstober/dotfiles/main/ansible.cfg .
-wget https://raw.githubusercontent.com/rstober/dotfiles/main/add-user.yaml .
-wget https://raw.githubusercontent.com/rstober/dotfiles/main/run-yum-update.yaml .
-wget https://raw.githubusercontent.com/rstober/dotfiles/main/clone-software-image.yaml .
-wget https://raw.githubusercontent.com/rstober/dotfiles/main/install-gnome-desktop.yaml .
-wget https://raw.githubusercontent.com/rstober/dotfiles/main/install-b4ds.yaml .
-wget https://raw.githubusercontent.com/rstober/dotfiles/main/clone-and-update-category.yaml .
-wget https://raw.githubusercontent.com/rstober/dotfiles/main/assign-nodes-to-category.yaml .
-wget https://raw.githubusercontent.com/rstober/dotfiles/main/configure-slurm.yaml .
-wget https://raw.githubusercontent.com/rstober/dotfiles/main/configure-auto-scaler.yaml .
-wget https://raw.githubusercontent.com/rstober/dotfiles/main/configure-usemarketplaceamis.yaml .
+wget https://raw.githubusercontent.com/rstober/dotfiles/main/cmshrc
+wget https://raw.githubusercontent.com/rstober/dotfiles/main/bookmarks-cmsh
+wget https://raw.githubusercontent.com/rstober/dotfiles/main/du.cmsh
+wget https://raw.githubusercontent.com/rstober/dotfiles/main/si.cmsh
+wget https://raw.githubusercontent.com/rstober/dotfiles/main/ansible.cfg
+wget https://raw.githubusercontent.com/rstober/dotfiles/main/add-user.yaml
+wget https://raw.githubusercontent.com/rstober/dotfiles/main/run-yum-update.yaml
+wget https://raw.githubusercontent.com/rstober/dotfiles/main/clone-software-image.yaml
+wget https://raw.githubusercontent.com/rstober/dotfiles/main/install-gnome-desktop.yaml
+wget https://raw.githubusercontent.com/rstober/dotfiles/main/install-b4ds.yaml
+wget https://raw.githubusercontent.com/rstober/dotfiles/main/clone-and-update-category.yaml
+wget https://raw.githubusercontent.com/rstober/dotfiles/main/assign-nodes-to-category.yaml
+wget https://raw.githubusercontent.com/rstober/dotfiles/main/configure-slurm.yaml
+wget https://raw.githubusercontent.com/rstober/dotfiles/main/configure-auto-scaler.yaml
+wget https://raw.githubusercontent.com/rstober/dotfiles/main/configure-usemarketplaceamis.yaml
+wget https://raw.githubusercontent.com/rstober/dotfiles/main/install-jupyter.yaml
+wget https://raw.githubusercontent.com/rstober/dotfiles/main/cm-jupyter-setup.conf
 
 # download playbooks
 
@@ -36,6 +38,7 @@ cp bookmarks-cmsh /root/.bookmarks-cmsh
 cp du.cmsh /root/.cm/cmsh/du.cmsh
 cp si.cmsh /root/.cm/cmsh/si.cmsh
 cp ansible.cfg /root/.ansible.cfg
+cp cm-jupyter-setup.conf /root/cm-jupyter-setup.conf
 
 # install the brightcomputing.bcm collection
 module load python3
@@ -73,6 +76,10 @@ ansible-playbook -ilocalhost, --flush-cache ${installdir}/configure-auto-scaler.
 
 # allow use of marketplace amis
 ansible-playbook -ilocalhost, --flush-cache ${installdir}/configure-usemarketplaceamis.yaml
+
+# install Jupyter
+# allow use of marketplace amis
+ansible-playbook -ilocalhost, --flush-cache ${installdir}/install-jupyter.yaml
 
 # create a set of users
 for user in robert david alice bob charlie edgar frank
