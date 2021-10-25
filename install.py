30cd1bc57410a0279bf5074952106d4ba6b73298
Robert Stober <rstober@users.noreply.github.com>
2021-10-25 09:56:30 -0800

    Add files via upload

--- install.py
+++ install.py
@@ -8,22 +8,22 @@ install_dir = "/root/.road-runner"
 
 if __name__ == '__main__':
 
-    # delete the installation directory if it exists
-    isExist = os.path.exists(install_dir)
-    if isExist:
-        shutil.rmtree(install_dir)
+    # # delete the installation directory if it exists
+    # isExist = os.path.exists(install_dir)
+    # if isExist:
+        # shutil.rmtree(install_dir)
     
-    # create the installation director
-    os.mkdir(install_dir)
+    # # create the installation director
+    # os.mkdir(install_dir)
     
-    # always cd into install_dir
-    os.chdir(install_dir)
+    # # always cd into install_dir
+    # os.chdir(install_dir)
     
-    # install git
-    os.system("yum install -y git")
+    # # install git
+    # os.system("yum install -y git")
     
-    # install road-runner distribution
-    os.system("git clone https://github.com/rstober/dotfiles.git %s" % install_dir)
+    # # install road-runner distribution
+    # os.system("git clone https://github.com/rstober/dotfiles.git %s" % install_dir)
     
     # load the python3 module
     exec(open('/cm/local/apps/environment-modules/4.5.3/Modules/default/init/python.py').read())
@@ -34,35 +34,35 @@ if __name__ == '__main__':
     stream = open("install_config.yaml", 'r')
     dictionary = yaml.full_load(stream)
     
-    # install ansible base
-    os.system('pip install ansible==' + dictionary["ansible_version"])
+    # # install ansible base
+    # os.system('pip install ansible==' + dictionary["ansible_version"])
     
-    # install the brightcomputing.bcm Ansible collection
-    os.system("ansible-galaxy collection install brightcomputing.bcm")
+    # # install the brightcomputing.bcm Ansible collection
+    # os.system("ansible-galaxy collection install brightcomputing.bcm")
    
-    # copy the CMSH aliases, bookmarks and scriptlets to their proper locations
-    shutil.copyfile("cmshrc", "/root/.cmshrc")
-    shutil.copyfile("bookmarks-cmsh", "/root/.bookmarks-cmsh")
-    shutil.copyfile("du.cmsh", "/root/.cm/cmsh/du.cmsh")
-    shutil.copyfile("cu.cmsh", "/root/.cm/cmsh/cu.cmsh")
-    shutil.copyfile("si.cmsh", "/root/.cm/cmsh/si.cmsh")
-    shutil.copyfile("dp.cmsh", "/root/.cm/cmsh/dp.cmsh")
-    shutil.copyfile("ansible.cfg", "/root/.ansible.cfg")
+    # # copy the CMSH aliases, bookmarks and scriptlets to their proper locations
+    # shutil.copyfile("cmshrc", "/root/.cmshrc")
+    # shutil.copyfile("bookmarks-cmsh", "/root/.bookmarks-cmsh")
+    # shutil.copyfile("du.cmsh", "/root/.cm/cmsh/du.cmsh")
+    # shutil.copyfile("cu.cmsh", "/root/.cm/cmsh/cu.cmsh")
+    # shutil.copyfile("si.cmsh", "/root/.cm/cmsh/si.cmsh")
+    # shutil.copyfile("dp.cmsh", "/root/.cm/cmsh/dp.cmsh")
+    # shutil.copyfile("ansible.cfg", "/root/.ansible.cfg")
     
-    # download and install the AWS CLI if Jupyter is going to be deployed
-    if dictionary["deploy_jupyter"]:
-        os.system("curl \"https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip\" -o \"awscliv2.zip\"")
-        shutil.unpack_archive('awscliv2.zip', install_dir, 'zip')
-        os.chmod("aws/install", stat.S_IEXEC)
-        os.chmod("aws/dist/aws", stat.S_IEXEC)
-        os.system("./aws/install")
+    # # download and install the AWS CLI if Jupyter is going to be deployed
+    # if dictionary["deploy_jupyter"]:
+        # os.system("curl \"https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip\" -o \"awscliv2.zip\"")
+        # shutil.unpack_archive('awscliv2.zip', install_dir, 'zip')
+        # os.chmod("aws/install", stat.S_IEXEC)
+        # os.chmod("aws/dist/aws", stat.S_IEXEC)
+        # os.system("./aws/install")
     
-    if dictionary["update_head_node"]:    
-        os.environ['ANSIBLE_PYTHON_INTERPRETER'] = '/usr/bin/python'
-        os.system("ansible-playbook -ilocalhost, dnf-update.yaml")
+    # if dictionary["update_head_node"]:    
+        # os.environ['ANSIBLE_PYTHON_INTERPRETER'] = '/usr/bin/python'
+        # os.system("ansible-playbook -ilocalhost, dnf-update.yaml")
         
     if dictionary["categories"]:
         for category in dictionary["categories"]:
-            print("category: " + category)
+            print("category: " + category.name)
         
      
