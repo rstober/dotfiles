f6c50dc940017b81a0eff68ee00d88fc6062502a
Robert Stober <rstober@users.noreply.github.com>
2021-10-24 18:33:59 -0800

    Add files via upload

--- install.py
+++ install.py
@@ -24,23 +24,37 @@ if __name__ == '__main__':
     # install road-runner distribution
     os.system("git clone https://github.com/rstober/dotfiles.git %s" % install_dir)
     
+    # read in client configuration
     stream = open("install_config.yaml", 'r')
     dictionary = yaml.full_load(stream)
-    
-    os.system("curl \"https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip\" -o \"awscliv2.zip\"")
     
     # load the python3 module
     exec(open('/cm/local/apps/environment-modules/4.5.3/Modules/default/init/python.py').read())
     module('load','python3')
     module('list')
     
+    # install ansible base
     os.system('pip install ansible==' + dictionary["ansible_version"])
+    
+    # install the brightcomputing.bcm Ansible collection
+    os.system("ansible-galaxy collection install brightcomputing.bcm")
     
     # filelist = glob.glob(install_dir + '/*')
     # for f in filelist:
         # print(f)
     
+    shutil.copyfile("cmshrc", "/root/.cmshrc")
+    shutil.copyfile("bookmarks-cmsh", "/root/.bookmarks-cmsh")
+    shutil.copyfile("du.cmsh", "/root/.cm/cmsh/du.cmsh")
+    shutil.copyfile("cu.cmsh", "/root/.cm/cmsh/cu.cmsh")
+    shutil.copyfile("si.cmsh", "/root/.cm/cmsh/si.cmsh")
+    shutil.copyfile("dp.cmsh", "/root/.cm/cmsh/dp.cmsh")
+    shutil.copyfile("ansible.cfg", "/root/.ansible.cfg")
     
+    if deploy_jupyter:
+        # download the AWS CLI
+        os.system("curl \"https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip\" -o \"awscliv2.zip\"")
+        shutil.unpack_archive(awscliv2.zip, install_dir, zip)
     
     
     #print(dictionary["cloned_software_image_name"])
